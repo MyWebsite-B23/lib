e74f887d2d6113a5cbf08b5e4b33dafef4397146
BharathGowdaB <bharathvishnubharath@gmail.com>
2024-12-02 09:42:39 +0530

    export fix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import DynamoDB from './Dynamodb/util';
 import  SchemaDefinitions from './Schema/definition.json';
-export * from './Dynamodb/util';
 
 export const getStandardSchemaDefinition = () => {
   return SchemaDefinitions;
 }
+
+export  { DynamoDB };
