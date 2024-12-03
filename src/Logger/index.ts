33b1564ade7717352685586f3662df004453cd58
BharathGowdaB <bharathvishnubharath@gmail.com>
2024-12-03 10:15:46 +0530

    updated loger'

--- src/Logger/index.ts
+++ src/Logger/index.ts
@@ -1,16 +1,20 @@
 import util from 'util';
 const Logger = {
-    logException: (functionName: string, error: any) => {
-        console.log(`Exception Occurred in Function: ${functionName}, Error: ${util.inspect(error)}`);
-    },
+  logException: (functionName: string, error: any) => {
+    console.log(`Exception Occurred in Function: ${functionName}, Error: ${util.inspect(error)}`);
+  },
 
-    logError: (functionName: string, errorMessage: string) => {
-        console.log(`Error Occurred in Function: ${functionName}, Error: ${util.inspect(errorMessage)}`);
-    },
+  logError: (functionName: string, errorMessage: string) => {
+    console.log(`Error Occurred in Function: ${functionName}, Error: ${util.inspect(errorMessage)}`);
+  },
 
-    logInvalidPayload: (functionName: string, errorMessage: string) => {
-        console.log(`Invalid Payload received for Function: ${functionName}, Error: ${errorMessage}`);
-    }
+  logMessage: (functionName: string, message: any) => {
+    console.log(`Message in Function: ${functionName}, Error: ${util.inspect(message)}`);
+  },
+
+  logInvalidPayload: (functionName: string, errorMessage: string) => {
+    console.log(`Invalid Payload received for Function: ${functionName}, Error: ${errorMessage}`);
+  }
 }
 
 export default Logger;
