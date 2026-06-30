f26daded22cd7c5953d89ce138cd9c663800b062
BharathGowdaB <bharathvishnubharath@gmail.com>
2026-06-30 10:10:45 +0530

    fixed schema export

--- src/Schema/index.ts
+++ src/Schema/index.ts
@@ -1,9 +1,11 @@
 import SchemaDefinitions from './definition';
+import TieredPriceSchema from './tieredPrice';
 
 const Schema = {
   getStandardSchemaDefinition() {
     return SchemaDefinitions;
-  }
+  },
+  TieredPriceSchema,
 }
 
 export default Schema;
